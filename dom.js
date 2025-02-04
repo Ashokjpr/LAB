2035615bf7ec3d9c9c028bd8c08a27e4e00642bc
Ashokjpr <meenaashokkumar260@gmail.com>
2025-02-04 17:14:32 +0530

    Calculator

---
+++ dom.js
@@ -0,0 +1,9 @@
+let h1 = document.getElementById("head")
+console.log(h1)
+h1.innerText = "my form"
+let Fname = document.getElementById("Fname")
+console.log(Fname)
+let mob = document.getElementById("mob")
+console.log(mob)
+let age = document.getElementById("age")
+console.log(age)
---
+++ js.js
@@ -0,0 +1,9 @@
+let h1 = document.getElementById("heding")
+console.log(h1)
+
+
+let p = document.getElementsByClassName("para")
+console.log(p)
+
+let fname = document.getElementsByName("gendar")
+console.log(fname)
