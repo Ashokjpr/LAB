3982024e45c9afe45155c261b8545112161d9e2b
Ashokjpr <meenaashokkumar260@gmail.com>
2025-02-06 10:57:53 +0530

    UPDATE

--- js.js
+++
@@ -1,9 +0,0 @@
-let h1 = document.getElementById("heding")
-console.log(h1)
-
-
-let p = document.getElementsByClassName("para")
-console.log(p)
-
-let fname = document.getElementsByName("gendar")
-console.log(fname)
